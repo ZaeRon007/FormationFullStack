85b3f80eafdfc54e6b317fa391cbf5d3064a73f8
ZaeRon007 <julien.pelissier8@orange.fr>
2024-07-24 11:25:00 +0200

    Work in progress

--- projet 2/src/app/core/services/olympic.service.ts
+++ projet 2/src/app/core/services/olympic.service.ts
@@ -1,19 +1,24 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
-export class OlympicService {
-  private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+export class OlympicService implements OnInit{
+  private olympicUrl!: string;
+  private olympics$ = new BehaviorSubject<any>(undefined);//???
 
   constructor(private http: HttpClient) {}
 
+  ngOnInit(): void {
+    this.olympicUrl = './assets/mock/olympic.json';
+
+  }
+
   loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
+    return this.http.get<string>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
       catchError((error, caught) => {
         // TODO: improve error handling
